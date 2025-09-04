8ce5297e346f644d03206b1cfea827c6f1bceb1b
khindrisurbhi-hub <khindri.surbhi@gmail.com>
2025-09-04 16:47:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 // config.js
 const BACKEND_URL = "https://ai-task-agent-backend.onrender.com";
-
 const CLIENT_ID = "943220084061-ujlua6j24n7amv6tuufhtuaueh5rbf00.apps.googleusercontent.com";
-
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest"];
 const SCOPES = "https://www.googleapis.com/auth/tasks";
