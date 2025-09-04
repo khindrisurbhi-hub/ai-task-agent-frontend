94a5f83ccfe82a3ec9a9cfa609f692605c809532
khindrisurbhi-hub <khindri.surbhi@gmail.com>
2025-09-04 13:43:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,5 @@ const BACKEND_URL = "https://ai-task-agent-backend.onrender.com";
 
 const CLIENT_ID = "943220084061-ujlua6j24n7amv6tuufhtuaueh5rbf00.apps.googleusercontent.com";
 
-// Replace this with your real API key from Google Cloud
-const API_KEY = "YOUR_API_KEY_HERE";
-
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest"];
 const SCOPES = "https://www.googleapis.com/auth/tasks";
