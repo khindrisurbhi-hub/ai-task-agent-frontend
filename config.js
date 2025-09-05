f832c03bdd830a5476188c5dbf7395aeb8c4a435
khindrisurbhi-hub <khindri.surbhi@gmail.com>
2025-09-05 17:07:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,16 @@
 // config.js
-const BACKEND_URL = "https://ai-task-agent-backend.onrender.com";
 const CLIENT_ID = "943220084061-ujlua6j24n7amv6tuufhtuaueh5rbf00.apps.googleusercontent.com";
+
+const SCOPES = [
+  "https://www.googleapis.com/auth/tasks",
+  "https://www.googleapis.com/auth/calendar.readonly",
+  "https://www.googleapis.com/auth/gmail.readonly",
+  "https://www.googleapis.com/auth/contacts.readonly"
+].join(" ");
 
 const DISCOVERY_DOCS = [
   "https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest",
   "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
   "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest",
-  "https://www.googleapis.com/discovery/v1/apis/people/v1/rest"
+  "https://people.googleapis.com/$discovery/rest?version=v1"
 ];
-
-const SCOPES = `
-  https://www.googleapis.com/auth/tasks
-  https://www.googleapis.com/auth/calendar.readonly
-  https://www.googleapis.com/auth/gmail.readonly
-  https://www.googleapis.com/auth/contacts.readonly
-`;
