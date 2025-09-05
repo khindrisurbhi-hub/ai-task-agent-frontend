925cd026856619c93a68e14638bef67ab79fdf28
khindrisurbhi-hub <khindri.surbhi@gmail.com>
2025-09-05 18:38:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,16 @@
-// ✅ OAuth 2.0 Client ID
+// config.js
 const CLIENT_ID = "943220084061-ujlua6j24n7amv6tuufhtuaueh5rbf00.apps.googleusercontent.com";
 
-const SCOPES = `
-  https://www.googleapis.com/auth/tasks
-  https://www.googleapis.com/auth/calendar.readonly
-  https://www.googleapis.com/auth/gmail.readonly
-  https://www.googleapis.com/auth/contacts.readonly
-`;
+const SCOPES = [
+  "https://www.googleapis.com/auth/tasks",
+  "https://www.googleapis.com/auth/calendar.readonly",
+  "https://www.googleapis.com/auth/gmail.readonly",
+  "https://www.googleapis.com/auth/contacts.readonly"
+].join(" ");
 
-export { CLIENT_ID, SCOPES };
+const DISCOVERY_DOCS = [
+  "https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest",
+  "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
+  "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest",
+  "https://www.googleapis.com/discovery/v1/apis/people/v1/rest"
+];
