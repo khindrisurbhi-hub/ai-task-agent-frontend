fa4f8e76f514495c2c1d24203ffd83683affd13e
khindrisurbhi-hub <khindri.surbhi@gmail.com>
2025-09-05 19:02:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
 // config.js
+
+// ✅ Use your OAuth 2.0 Client ID
 const CLIENT_ID = "943220084061-ujlua6j24n7amv6tuufhtuaueh5rbf00.apps.googleusercontent.com";
 
+// ✅ Required scopes (Tasks + Calendar + Gmail + Contacts)
 const SCOPES = [
   "https://www.googleapis.com/auth/tasks",
   "https://www.googleapis.com/auth/calendar.readonly",
   "https://www.googleapis.com/auth/gmail.readonly",
   "https://www.googleapis.com/auth/contacts.readonly"
-].join(" ");
-
-const DISCOVERY_DOCS = [
-  "https://www.googleapis.com/discovery/v1/apis/tasks/v1/rest",
-  "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
-  "https://www.googleapis.com/discovery/v1/apis/gmail/v1/rest",
-  "https://www.googleapis.com/discovery/v1/apis/people/v1/rest"
 ];
